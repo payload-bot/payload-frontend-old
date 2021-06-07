91c265d9b7a01950e1ce7a8f192b1d46608b49f7
c43721 <rlahman.game@gmail.com>
2021-06-06 22:10:32 -0500

    feat!: Use NEXT_PUBLIC prefix to ENV, add redirects

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const getBackendHost = () =>
-  process.env.NODE_ENV === 'production'
+  process.env.NEXT_PUBLIC_ENV === 'production'
     ? 'https://api.payload.tf'
     : 'http://localhost'
 
@@ -11,6 +11,21 @@ module.exports = {
       {
         source: '/api/:urls*',
         destination: `${host}/api/:urls*`,
+      },
+    ]
+  },
+
+  async redirects() {
+    return [
+      {
+        source: '/invite',
+        destination: 'https://discordapp.com/oauth2/authorize?client_id=644333502870978564&permissions=388161&scope=bot',
+        permanent: true,
+      },
+      {
+        source: '/discord',
+        destination: 'https://discord.com/invite/gYnnMYz',
+        permanent: true,
       },
     ]
   },
