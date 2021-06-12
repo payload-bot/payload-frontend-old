75b8693d106b7895f20daf22b2743ac3d0bbf2da
c43721 <rlahman.game@gmail.com>
2021-06-12 13:44:56 -0500

    feat: Add refresh token storage, redo auth fail page

--- src/pages/login/failure.tsx
+++ src/pages/login/failure.tsx
@@ -1,11 +1,22 @@
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { Container, Box, Typography } from '@material-ui/core'
+import Layout from '../../components/layout/Layout'
 
 export default function failure() {
   const [errorMessage, setErrorMessage] = useState('Error while authenticating')
+
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search)
     setErrorMessage(urlParams.get('message'))
   }, [])
 
-  return <div>{errorMessage}</div>
+  return (
+    <Layout>
+      <Container>
+        <Box display="flex" alignItems="center" justifyContent="center">
+          <Typography variant="h4">{errorMessage}</Typography>
+        </Box>
+      </Container>
+    </Layout>
+  )
 }
