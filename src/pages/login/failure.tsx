95b5d6e6b2bdf437eb0c0922797da7f036d2f99f
c43721 <rlahman.game@gmail.com>
2021-06-13 19:46:29 -0500

    feat: Add link back to home on login failure

--- src/pages/login/failure.tsx
+++ src/pages/login/failure.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import { Container, Box, Typography } from '@material-ui/core'
+import Link from 'next/link'
+import { Container, Box, Typography, Button } from '@material-ui/core'
 import Layout from '../../components/layout/Layout'
 
 export default function failure() {
@@ -15,6 +16,11 @@ export default function failure() {
       <Container>
         <Box display="flex" alignItems="center" justifyContent="center">
           <Typography variant="h4">{errorMessage}</Typography>
+          <Link href="/">
+            <Button variant="outlined" color="primary">
+              Take me back
+            </Button>
+          </Link>
         </Box>
       </Container>
     </Layout>
