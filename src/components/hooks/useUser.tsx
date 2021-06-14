da6fccd1780a2439c79e6318ecd2acd76aeb61d3
c43721 <rlahman.game@gmail.com>
2021-06-14 08:37:54 -0500

    feat: Route to home after logout

--- src/components/hooks/useUser.tsx
+++ src/components/hooks/useUser.tsx
@@ -1,15 +1,18 @@
+import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { useAppSelector } from '../../redux/store'
 import { fetchUser, logoutUser } from '../../redux/users/userSlice'
 
 export default function useUser() {
+  const router = useRouter()
   const dispatch = useDispatch()
 
   const logout = () => {
     localStorage.removeItem('token')
     localStorage.removeItem('refresh_token')
     dispatch(logoutUser())
+    router.push('/')
   }
 
   const { user, loggedIn, loading, isAdmin } = useAppSelector(
