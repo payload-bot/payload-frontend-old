36a0c1d3874493e697dd8f28ada12adb5190f9d9
c43721 <rlahman.game@gmail.com>
2021-06-14 22:55:59 -0500

    feat: Remove refresh tokens after 1 week

--- src/components/hooks/useAuth.tsx
+++ src/components/hooks/useAuth.tsx
@@ -1,17 +1,19 @@
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
+import useLocalStorage from './useLocalStorage'
 import useUser from './useUser'
 
 export default function useAuth(admin?: boolean) {
   const router = useRouter()
+  const [token] = useLocalStorage('token', null)
   const { loading, loggedIn, user, isAdmin } = useUser()
 
   useEffect(() => {
+    if (!token) router.push('/')
     if (loading) return
+    if (user === null) router.push('/')
     if (admin && !isAdmin) router.push('/')
-
-    if (!loading && user === null) router.push('/')
-  }, [loading, user, admin, isAdmin])
+  }, [loading, user, admin, isAdmin, token])
 
   return [loggedIn]
 }
