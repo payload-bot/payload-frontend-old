62f11dfd09fef3c288bfebc3ddb6099591337fc1
c43721 <rlahman.game@gmail.com>
2021-07-11 18:20:35 -0500

    feat(GuildSettings): Add page for setting guild settings

--- src/components/DashboardSidebar.tsx
+++ src/components/DashboardSidebar.tsx
@@ -3,9 +3,9 @@ import {
   ListItem,
   ListItemIcon,
   ListItemText,
-  Divider,
+  Box,
+  makeStyles,
 } from '@material-ui/core'
-import { useState } from 'react'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { ActiveServer } from '../redux/servers/types'
@@ -13,29 +13,52 @@ import { ActiveServer } from '../redux/servers/types'
 import SettingsIcon from '@material-ui/icons/Settings'
 import BuildIcon from '@material-ui/icons/Build'
 import HomeIcon from '@material-ui/icons/Home'
+import BackIcon from '@material-ui/icons/ArrowBack'
+
+const useStyles = makeStyles(theme => ({
+  backButton: {
+    marginTop: 'auto',
+  },
+}))
 
 export default function DashboardSidebar(props: ActiveServer) {
   const router = useRouter()
+  const styles = useStyles()
 
   return (
     <>
       <List component="aside" aria-label="main sidebar">
-        <Link href={`/dashboard/${props.id}`}>
-          <ListItem button selected={router.pathname === '/dashboard/[id]'}>
-            <ListItemIcon>
-				<HomeIcon />
-            </ListItemIcon>
-            <ListItemText primary="Home" />
-          </ListItem>
-        </Link>
-        {/* <Link href={`/dashboard/${props.id}/settings`}>
-          <ListItem button selected={router.pathname === '/dashboard/[id]/settings'}>
-            <ListItemIcon>
-              <BuildIcon />
-            </ListItemIcon>
-            <ListItemText primary="Configure" />
-          </ListItem>
-        </Link> */}
+        <Box display="flex" flexDirection="column">
+          <Link href={`/dashboard/${props.id}`}>
+            <ListItem button selected={router.pathname === '/dashboard/[id]'}>
+              <ListItemIcon>
+                <HomeIcon />
+              </ListItemIcon>
+              <ListItemText primary="Home" />
+            </ListItem>
+          </Link>
+          <Link href={`/dashboard/${props.id}/settings`}>
+            <ListItem
+              button
+              selected={router.pathname === '/dashboard/[id]/settings'}
+            >
+              <ListItemIcon>
+                <BuildIcon />
+              </ListItemIcon>
+              <ListItemText primary="Configure" />
+            </ListItem>
+          </Link>
+          <Box display="flex" flexGrow={1}></Box>
+          <Link href={`/dashboard`}>
+            {/* Lol, this is never going to happen... */}
+            <ListItem button selected={router.pathname === '/dashboard'}>
+              <ListItemIcon>
+                <BackIcon />
+              </ListItemIcon>
+              <ListItemText primary="Back" />
+            </ListItem>
+          </Link>
+        </Box>
       </List>
     </>
   )
---
+++ src/pages/dashboard/[id]/settings.tsx
@@ -0,0 +1,259 @@
+import { useRouter } from 'next/router'
+import withAuth from '../../../components/withAuth'
+import Layout from '../../../components/layout/Layout'
+import { useDispatch } from 'react-redux'
+import React, { useEffect, useState } from 'react'
+import { fetchServer, updateServer } from '../../../redux/servers/serverSlice'
+import { useAppSelector } from '../../../redux/store'
+import {
+  CircularProgress,
+  Container,
+  makeStyles,
+  Typography,
+  Box,
+  Card,
+  CardContent,
+  TextField,
+  MenuItem,
+  Select,
+  Button,
+  Snackbar,
+} from '@material-ui/core'
+import ServerAvatar from '../../../components/ServerAvatar'
+import { Controller, useForm } from 'react-hook-form'
+import { ActiveServer } from '../../../redux/servers/types'
+import Alert from '../../../components/Alert'
+import DashboardSidebar from '../../../components/DashboardSidebar'
+
+const useStyles = makeStyles(theme => ({
+  card: {
+    minWidth: '35%',
+  },
+}))
+
+function ServerDashboardPage() {
+  const router = useRouter()
+  const { id } = router.query
+  const dispatch = useDispatch()
+  const styles = useStyles()
+  const {
+    activeServer,
+    activeServerId,
+    passedBetaCheck,
+    loadingActiveServer,
+    loadingActiveServerErrorMsg,
+    loadedServerCache,
+    updateActiveServerErrorMsg,
+  } = useAppSelector(state => state.servers)
+
+  const [serverUpdateSuccess, setUpdateServerSuccess] = useState(false)
+  const [serverUpdateFailure, setUpdateServerFailure] = useState(false)
+
+  const {
+    control,
+    formState: { errors },
+    watch,
+    handleSubmit,
+  } = useForm()
+
+  const watchPrefix = watch('prefix')
+
+  const onSubmit = (data: Partial<ActiveServer>) => {
+    dispatch(updateServer(id as string, data))
+    if (!updateActiveServerErrorMsg) {
+      setUpdateServerSuccess(true)
+    } else {
+      setUpdateServerFailure(true)
+    }
+  }
+
+  useEffect(() => {
+    if (loadingActiveServerErrorMsg) {
+      router.push('/dashboard')
+    }
+  }, [loadingActiveServerErrorMsg])
+
+  useEffect(() => {
+    if (!loadedServerCache[id as string]) {
+      dispatch(fetchServer(id as string))
+    }
+  }, [activeServerId])
+
+  return (
+    <Layout sideBarEnabled={!loadingActiveServer} sideBarContent={<DashboardSidebar {...activeServer} />}>
+      <Container>
+        {loadingActiveServer && (
+          <Container>
+            <Box
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              flexDirection="column"
+              height="35vh"
+            >
+              <CircularProgress size={40} />
+              <Typography variant="h5">Loading your dashboard</Typography>
+            </Box>
+          </Container>
+        )}
+
+        {!loadingActiveServer && !passedBetaCheck && (
+          <Container>
+            <Box
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              height="35vh"
+            >
+              <Typography>You are not a beta tester :(</Typography>
+            </Box>
+          </Container>
+        )}
+
+        {!loadingActiveServer &&
+          passedBetaCheck &&
+          loadingActiveServerErrorMsg && (
+            <Container>
+              <Box
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                height="35vh"
+              >
+                <Typography>{loadingActiveServerErrorMsg}</Typography>
+              </Box>
+            </Container>
+          )}
+
+        {!loadingActiveServer && passedBetaCheck && activeServer && (
+          <Container>
+            <Box
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              flexDirection="column"
+              py={2}
+            >
+              <ServerAvatar icon={activeServer.icon} name={activeServer.name} />
+              <Typography variant="h6">{activeServer.name}</Typography>
+            </Box>
+            <Card className={styles.card}>
+              <CardContent>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                  <Typography variant="h5" style={{ margin: 8 }}>
+                    Bot name
+                  </Typography>
+                  <Controller
+                    name="botName"
+                    control={control}
+                    defaultValue={activeServer.botName}
+                    rules={{
+                      minLength: 1,
+                      maxLength: 100,
+                      required: 'Invalid nickname',
+                    }}
+                    render={({ field }) => (
+                      <TextField
+                        autoComplete="false"
+                        error={errors.botName ? true : false}
+                        helperText={errors.botName && errors.botName.message}
+                        {...field}
+                        style={{ margin: 8 }}
+                      />
+                    )}
+                  />
+                  <Typography variant="h5" style={{ margin: 8 }}>
+                    Prefix
+                  </Typography>
+                  <Controller
+                    name="prefix"
+                    control={control}
+                    defaultValue={activeServer.prefix}
+                    rules={{
+                      minLength: 1,
+                      maxLength: 75,
+                      required: 'You need a prefix!',
+                    }}
+                    render={({ field }) => (
+                      <TextField
+                        autoComplete="false"
+                        error={errors.prefix ? true : false}
+                        helperText={
+                          !!watchPrefix && !errors.botName
+                            ? `Usage: ${watchPrefix}commands`
+                            : errors.prefix
+                            ? errors.prefix.message
+                            : ''
+                        }
+                        {...field}
+                        style={{ margin: 8 }}
+                      />
+                    )}
+                  />
+                  <Typography variant="h5" style={{ margin: 8 }}>
+                    Language
+                  </Typography>
+                  <Controller
+                    name="language"
+                    control={control}
+                    defaultValue={activeServer.language}
+                    render={({ field }) => (
+                      <Select {...field} style={{ margin: 8 }}>
+                        <MenuItem value="en-US">English</MenuItem>
+                        <MenuItem value="es-ES">Spanish</MenuItem>
+                        <MenuItem value="fi-FI">Finnish</MenuItem>
+                        <MenuItem value="pl-PL">Polish</MenuItem>
+                      </Select>
+                    )}
+                  />
+                  <Typography variant="h5" style={{ margin: 8 }}>
+                    Snipe Permissions
+                  </Typography>
+                  <Controller
+                    name="enableSnipeForEveryone"
+                    control={control}
+                    defaultValue={activeServer.enableSnipeForEveryone}
+                    render={({ field }) => (
+                      <Select {...field} style={{ margin: 8 }}>
+                        <MenuItem value="true">Everyone</MenuItem>
+                        <MenuItem value="false">Admins</MenuItem>
+                      </Select>
+                    )}
+                  />
+                  <br />
+                  <Box mt={3}>
+                    <Button type="submit" color="primary" variant="outlined">
+                      Save
+                    </Button>
+                  </Box>
+                </form>
+              </CardContent>
+            </Card>
+          </Container>
+        )}
+      </Container>
+
+      {/* Success snackbar */}
+      <Snackbar
+        open={serverUpdateSuccess}
+        autoHideDuration={5000}
+        onClose={() => setUpdateServerSuccess(prev => !prev)}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+      >
+        <Alert severity="success">Successfully updated settings</Alert>
+      </Snackbar>
+
+      {/* Failure snackbar */}
+      <Snackbar
+        open={serverUpdateFailure}
+        autoHideDuration={5000}
+        onClose={() => setUpdateServerFailure(prev => !prev)}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+      >
+        <Alert severity="error">Failed to update settings</Alert>
+      </Snackbar>
+    </Layout>
+  )
+}
+
+export default withAuth(ServerDashboardPage)
