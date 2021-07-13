a036ef51a38752689e020e8e36f75dfc9b3fc869
c43721 <rlahman.game@gmail.com>
2021-07-12 21:37:02 -0500

    fix(styles): Make footer paper color

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import { Box, Theme, Typography } from '@material-ui/core'
+import { Box, Theme, Paper, Typography } from '@material-ui/core'
 import { makeStyles } from '@material-ui/styles'
 
 const useStyles = makeStyles((theme: Theme) => ({
@@ -8,7 +8,6 @@ const useStyles = makeStyles((theme: Theme) => ({
   },
   footer: {
     flexShrink: 1,
-    backgroundColor: '#23272a',
   },
 }))
 
@@ -17,36 +16,38 @@ export default function Footer() {
 
   return (
     <Box className={styles.footer}>
-      <Typography
-        variant="subtitle1"
-        align="center"
-        className={styles.spacingTop}
-      >
-        Based on{' '}
-        <a className="link no-underline" href="https://sharky.cool">
-          sharky's
-        </a>{' '}
-        bot, now developed by{' '}
-        <a
-          className="link no-underline"
-          href="https://steamcommunity.com/id/chab133"
+      <Paper>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          className={styles.spacingTop}
         >
-          24
-        </a>
-        . Proudly{' '}
-        <a className="link" href="https://github.com/payload-bot">
-          Open-Sourced
-        </a>
-        <br />
-        <a href="https://vercel.com/?utm_source=payload&utm_compaign=oss">
-          <Image
-            src="/img/vercel-logo.svg"
-            alt="Powered by Vercel Logo"
-            width={175}
-            height={75}
-          />
-        </a>
-      </Typography>
+          Based on{' '}
+          <a className="link no-underline" href="https://sharky.cool">
+            sharky's
+          </a>{' '}
+          bot, now developed by{' '}
+          <a
+            className="link no-underline"
+            href="https://steamcommunity.com/id/chab133"
+          >
+            24
+          </a>
+          . Proudly{' '}
+          <a className="link" href="https://github.com/payload-bot">
+            Open-Sourced
+          </a>
+          <br />
+          <a href="https://vercel.com/?utm_source=payload&utm_compaign=oss">
+            <Image
+              src="/img/vercel-logo.svg"
+              alt="Powered by Vercel Logo"
+              width={175}
+              height={75}
+            />
+          </a>
+        </Typography>
+      </Paper>
     </Box>
   )
 }
