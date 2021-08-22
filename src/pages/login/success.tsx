80716be7377f020117deec57e71f755aa85a0e07
c43721 <55610086+c43721@users.noreply.github.com>
2021-08-22 18:42:20 -0500

    fix: fix react component name and bot name errors (#31)
    * fix: correct name on success component
    
    * fix: fix bot name errors screwing up prefix preview

--- src/pages/login/success.tsx
+++ src/pages/login/success.tsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import useLocalStorage from '../../components/hooks/useLocalStorage'
 
-export default function success() {
+export default function Success() {
   const router = useRouter()
   const [_token, setToken] = useLocalStorage('token', '')
   const [_refresh, setRefreshToken] = useLocalStorage('refresh_token', '')
